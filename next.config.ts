899f195a8ed4bb9bab8f19df756759d011221d16
MHND09 <131745373+MHND09@users.noreply.github.com>
2025-07-06 04:34:03 +0100

    refactor: update ESLint configuration

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,6 @@ const nextConfig: NextConfig = {
     ]
     
   },
-  ignoreBuildErrors: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
